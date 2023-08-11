d1d46c11cc2b9edcbcec3fffb406e9136c6fb62a
sachin-kiwi <sachin.bisht@kiwitech.com>
2023-08-11 16:43:18 +0530

    added send transaction

--- src/components/transaction.jsx
+++ src/components/transaction.jsx
@@ -1,6 +1,8 @@
 import { useSignMessage } from "wagmi";
-import { recoverMessageAddress } from "viem";
+import { parseEther, recoverMessageAddress } from "viem";
 import { useEffect, useState } from "react";
+import { useSendTransaction, usePrepareSendTransaction } from "wagmi";
+
 function SignMessage() {
   const message = "gm wagmi frens";
   const { data, isLoading, isSuccess, signMessage } = useSignMessage({
@@ -38,4 +40,26 @@ function SignMessage() {
   );
 }
 
-export default SignMessage;
+const SendTxn = () => {
+  const { data, isLoading, isSuccess, sendTransaction } = useSendTransaction({
+    to: "0xdC9310e98B0F5b65077b202d81fDd9de19F7Eb9D",
+    value: parseEther("0.01"),
+    onSettled(data, error) {
+      if (error !== null) {
+        console.log(error);
+        return;
+      }
+      console.log(`Txn Hash:${data.hash}`);
+    },
+  });
+
+  return (
+    <div>
+      <button onClick={() => sendTransaction()}>Send Transaction</button>
+      {isLoading && <div>Check Wallet</div>}
+      {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
+    </div>
+  );
+};
+
+export { SignMessage, SendTxn };
